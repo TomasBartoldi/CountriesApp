0402ed46a30eabad9be40b5e51507b8ab4534411
Tomás <bartoldit@gmail.com>
2022-12-03 02:07:38 -0300

    Refactoring Cards CSS

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,20 +6,19 @@ const Card = ({ name, flag, continent }) => {
     
     <div className='countrie-card'>
 
-      <div className='countrie-flag'>
+      <div className='countrie-flag-container'>
         <img className='countrie-flag' src={flag} alt="Img not found" width='130px' height='105px' />
       </div>
 
-      <div  className='countrie-name'>
-        <h3>{name}</h3>
-      </div>
-       
+      <div  className='countrie-name-container'>
+        <div className='countrie-name'>
+          <h3>{name}</h3>
+        </div>
 
-      <div  className='countrie-continent'>
-        <h5>Continent:  {continent}</h5>
-      </div>
-
-
+        <div  className='countrie-continent'>
+          <h5>Continent:  {continent}</h5>
+        </div>
+      </div>      
       
     </div>
   )
