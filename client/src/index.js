90c25a46993ab63f40b893813045f4dcda44ff47
Tomás <bartoldit@gmail.com>
2022-12-05 16:03:25 -0300

    deply

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import { store } from "./store";
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "https://localhost:3001";
+
 
 ReactDOM.render(
   <Provider store={store}>
